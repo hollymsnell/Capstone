1e8111b51e05d6640b6d7283ccc610e9d98e6f0c
hollymsnell <hollymsnell@gmail.com>
2022-04-28 18:49:32 -0600

    update has_many/belongs_to relationships in models

--- app/models/exercise_program.rb
+++ app/models/exercise_program.rb
@@ -1,2 +1,4 @@
 class ExerciseProgram < ApplicationRecord
+  belongs_to :programs
+  belongs_to :exercises
 end
--- app/models/program.rb
+++ app/models/program.rb
@@ -1,2 +1,5 @@
 class Program < ApplicationRecord
+  has_many :exercise_programs
+  belongs_to :user
+  has_many :exercises, through: :exercise_programs
 end
