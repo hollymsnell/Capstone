ba3874727a1cdb5017baa7e8bb17396911fcfbd9
hollymsnell <hollymsnell@gmail.com>
2022-05-05 19:54:29 -0600

    working authentication with various updates

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,50 @@
 class ExercisesController < ApplicationController
+  before_action :authenticate_admin, except: [:index, :show]
+  def index
+    all_exercises = Exercise.all
+    render json: all_exercises
+  end
+
+  def show
+    exercise = Exercise.find_by(id: params["id"])
+    render json: exercise
+  end
+
+  def create
+    exercise = Exercise.new(
+      name: params["name"],
+      instructions: params["instructions"],
+      frequency: params["frequency"],
+      image: params["image"]
+    )
+    if exercise.save
+      render json: exercise
+    else
+      render json: {error_messages: exercise.errors.full_messages},
+      status: 422
+    end
+  end
+
+  def update
+    exercise_id = params["id"]
+    exercise = Exercise.find(product_id)
+    exercise.name = params["name"] || exercise.name
+    exercise.instructions = params["instructions"] || exercise.instructions
+    exercise.frequency = params["frequency"] || exercise.frequency
+    exercise.image = params["image"] || exercise.image
+
+    if product.save
+      render json: exercise
+    else
+      render json: {error_messages: exercise.errors.full_messages},
+      status: 422
+    end
+  end
+
+  def destroy
+    exercise_id = params["id"]
+    exercise = Exercise.find_by(id: exercise_id)
+    exercise.destroy
+    render json: {message: "exercise destroyed"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+###exercise routes
+get "/exercises" => "exercises#index"
+post "/exercises" => "exercises#create"
+get "/exercises/:id" => "exercises#show"
+patch "/exercises/:id" => "exercises#update"
+delete "/exercises/:id" => "exercises#destroy"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+###user routes
+post "/users" => "users#create"
+
+###session routes
+post "/sessions" => "sessions#create"
+
+###program routes
+get "/programs" => "programs#index"
+post "/programs" => "programs#create"
+get "/programs/:id" => "programs#show"
+
+###exercise_program routes
+get "/exercise_programs" => "exercise_programs#index"
+post "/exercise_programs" => "exercise_programs#create"
+delete "/exercise_programs/:id" => "exercise_programs#destroy"
 end
