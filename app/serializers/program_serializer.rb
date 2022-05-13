f0f46c490981e04f00f764abec857a3f25762d06
hollymsnell <hollymsnell@gmail.com>
2022-05-12 18:35:15 -0600

    added title to serializer

--- app/serializers/program_serializer.rb
+++ app/serializers/program_serializer.rb
@@ -1,5 +1,5 @@
 class ProgramSerializer < ActiveModel::Serializer
-  attributes :id, :user_rating, :user_id
+  attributes :id, :user_rating, :user_id, :title
   has_many :exercise_programs
   belongs_to :user
   has_many :exercises, through: :exercise_programs
